07431e81ca9096eec2a8198bdbcbd161f5621402
renatodln123 <renato.brtkm@gmail.com>
2020-08-27 13:06:59 -0300

    desafio13

--- teste.js
+++ teste.js
@@ -1,19 +1,3 @@
-var calc = {
-    "+": function(x,y) {return x + y},
-    "-": function(x,y) {return x - y}
-}
+var numbers = ["R", "E", "N"];
 
-function operation(operator) {
-    return function(number1, number2) {
-        return calc[operator](number1, number2);
-    }
-}
-
-var number1 = 10;
-var number2 = 20;
-
-operationSignalSum = "+";
-
-sum = operation(operationSignalSum);
-
-console.log(sum(number1, number2));
+console.log(numbers.toString());
